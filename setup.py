4e01497c94ff6ef986a1780e7ba4b6f0474b8dac
Robert Henning <henning@cars.uchicago.edu>
2016-07-26 15:16:31 -0500

    Add meta as a dependency

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(name = 'utils',
     packages = find_packages('.'),
     package_dir = {'utils': 'utils'},
-    install_requires = ['plotly'],
+    install_requires = ['plotly', 'meta'],
     zip_safe = False)
