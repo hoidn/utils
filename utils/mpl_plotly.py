59d7f084244df8ace5bbe27e8873143b41cb9bce
oliver <oliver@D-10-155-95-56.dhcp4.washington.edu>
2016-11-29 13:06:46 -0800

    Added methods plt.xlim and plt.ylim

--- utils/mpl_plotly.py
+++ utils/mpl_plotly.py
@@ -75,6 +75,12 @@ class Figure(object):
     def set_ylabel(self, ylabel):
         self.yaxis['title'] = ylabel
 
+    def set_xlim(self, range_tuple):
+        self.xaxis['range'] = list(range_tuple)
+        
+    def set_ylim(self, range_tuple):
+        self.yaxis['range'] = list(range_tuple)
+
     def set_title(self, title):
         self.layout['title'] = title
 
@@ -91,12 +97,12 @@ class Figure(object):
             raise NotImplementedError
 
     def show(self,save=False):
+        imagestr=None
         data = self.traces
         fig = go.Figure(data = data, layout = go.Layout(**self.layout))
-        imagestr=None
-+       if save:
-+           imagestr='svg'
-+       py.iplot(fig,image=imagestr)
+        if save:
+            imagestr = 'avg'
+        py.iplot(fig, image = imagestr)
 
 class Plt(object):
     def __init__(self):
@@ -143,6 +149,12 @@ class Plt(object):
 
     def xscale(self, scale):
         self._get_global_plot().set_xscale(scale)
+
+    def ylim(self, range_tuple):
+        self._get_global_plot().set_ylim(range_tuple)
+
+    def xlim(self, range_tuple):
+        self._get_global_plot().set_xlim(range_tuple)
 
     def yscale(self, scale):
         self._get_global_plot().set_yscale(scale)
